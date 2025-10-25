20b2eceda2dc1b397a694603a5449aed71e329e0
hnkk <100778377+luunguyentankhai@users.noreply.github.com>
2025-10-26 00:31:10 +0700

    change name func call

--- main.py
+++ main.py
@@ -1,39 +1,40 @@
 # Made by Team 3
 # Student name : Lưu Nguyễn Tấn Khải SE203817
-# Student name : Nguyễn Nhật Huy SE203829
 # Student name : Nguyễn Phạm Mai Phương SE204019
 # Student name : Lê Nam Phong SE 204029
 # Lab1 subject PFP191
 
-#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-#@@@@@@@@@@@@@@@@@@@@@@@@~~~*@@@@@@@@@@@@@@@@@@@@@@
-#@@@@@@@@@@@@@@@@@@@@@8~~~$~~~o$@@@@@@@@@@i1@@@@@@@
-#@@@@@@@@@@@@@@@@@&3$$#~~~&&n&~&$$$$$$86@@@@$@@@@@@
-#@@@@@@@@@@@@@6$$$$$$$$8~~13&v~&$$$$$$$$#%@6zn@@@@@
-#@@@@@@@@@@3$$$$$$$$$$$$1~~~z~~$$$$$$$$&1na@i@@@@@@
-#@@@@@@@#8$$$$$$$$$$$$$##i~vi8$@@#&&&@&#1v3@31@@@@@
-#@@@@@@%$$$$$$$$$$#&#$$$$$$$$$$$$$$$$$$$@13%~@@@@@@
-#@@@@@%&&$$$$$#&$$$$$$$$$$$&@@&%8%$#@#$$$$#%81a@@@@
-#@@@@@@%&&$#&$$$$$$$##88!iiiiiiiiizzii883##%&i1@@@@
-#@@@@@@@&#@$$$$$&888izzz8aiiii3zizauii3ii3#&&@@&@@@
-#@@@@@@@@&&$$@883iiiazz3vuzzzzu%ziiiiii$iiu&@&@@@@@
-#@@@@@@@&&3#8$iiiiiiiii;.3ii6ii8#ii!iii8iii@@&&%@@@
-#@@@@@@@3&&8i!iii8iii6@..*1ii8ii6 8i6!i8iiiz@@&&&@@
-#@@@@@@@$&&i3iiiiii68 .....$ii@ii8 ..-. iii1#&6@@@@
-#@@@@@@@#&&i%iiiii$n6866$-...$8i.3$u33-%i3i6i8@@@@@
-#@@na&#&&&@i3iiiiii.------.......-----oiiii$iii@@@@
-#@@@@@@@@@iii%iii6ii!+-----;;~;~..----*iii8v#$ @@@@
-#@@@@@@iiiii88o8iii6&.....;;;;;;...... #$$$@-u @@@@
-#@@@@@@@@@iiii8%@88$ .....*;;;;u.....$$&$$8!o^v&1@@
-#@@@@@@!!!!!ii88@i6i!@@#1 . uo..+8$#3iz6#$$n!&.@.@@
-#@@@!!!6$8!!!!!8@@!8!!!&#&@%%%#&&#$iii16$$$!6.^.u@@
-#@@@@@@%%%!!8!!!!3%!!3!!!!@&&#&&$#i!!i!6&$$$$.. &@@
-#@@@@@@@@%%3!38!!866666@@$@888666#!!63%%#$$$$@@#@@@
-#@@@@@@@@@@@$3333%8666@$$666$$@$%@@@%%%&%%@@@@@@@@@
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+# @@@@@@@@@@@@@@@@@@@@@@@@~~~*@@@@@@@@@@@@@@@@@@@@@@
+# @@@@@@@@@@@@@@@@@@@@@8~~~$~~~o$@@@@@@@@@@i1@@@@@@@
+# @@@@@@@@@@@@@@@@@&3$$#~~~&&n&~&$$$$$$86@@@@$@@@@@@
+# @@@@@@@@@@@@@6$$$$$$$$8~~13&v~&$$$$$$$$#%@6zn@@@@@
+# @@@@@@@@@@3$$$$$$$$$$$$1~~~z~~$$$$$$$$&1na@i@@@@@@
+# @@@@@@@#8$$$$$$$$$$$$$##i~vi8$@@#&&&@&#1v3@31@@@@@
+# @@@@@@%$$$$$$$$$$#&#$$$$$$$$$$$$$$$$$$$@13%~@@@@@@
+# @@@@@%&&$$$$$#&$$$$$$$$$$$&@@&%8%$#@#$$$$#%81a@@@@
+# @@@@@@%&&$#&$$$$$$$##88!iiiiiiiiizzii883##%&i1@@@@
+# @@@@@@@&#@$$$$$&888izzz8aiiii3zizauii3ii3#&&@@&@@@
+# @@@@@@@@&&$$@883iiiazz3vuzzzzu%ziiiiii$iiu&@&@@@@@
+# @@@@@@@&&3#8$iiiiiiiii;.3ii6ii8#ii!iii8iii@@&&%@@@
+# @@@@@@@3&&8i!iii8iii6@..*1ii8ii6 8i6!i8iiiz@@&&&@@
+# @@@@@@@$&&i3iiiiii68 .....$ii@ii8 ..-. iii1#&6@@@@
+# @@@@@@@#&&i%iiiii$n6866$-...$8i.3$u33-%i3i6i8@@@@@
+# @@na&#&&&@i3iiiiii.------.......-----oiiii$iii@@@@
+# @@@@@@@@@iii%iii6ii!+-----;;~;~..----*iii8v#$ @@@@
+# @@@@@@iiiii88o8iii6&.....;;;;;;...... #$$$@-u @@@@
+# @@@@@@@@@iiii8%@88$ .....*;;;;u.....$$&$$8!o^v&1@@
+# @@@@@@!!!!!ii88@i6i!@@#1 . uo..+8$#3iz6#$$n!&.@.@@
+# @@@!!!6$8!!!!!8@@!8!!!&#&@%%%#&&#$iii16$$$!6.^.u@@
+# @@@@@@%%%!!8!!!!3%!!3!!!!@&&#&&$#i!!i!6&$$$$.. &@@
+# @@@@@@@@%%3!38!!866666@@$@888666#!!63%%#$$$$@@#@@@
+# @@@@@@@@@@@$3333%8666@$$666$$@$%@@@%%%&%%@@@@@@@@@
 
 from services.student_manager import StudentManager
+from services.calculating import Average_GPA
 from utils.validation import select_menu_choice
 from utils.auth import authenticate_user
+
 
 def output_student_list(manager, Slist):
     if not Slist:
@@ -43,11 +44,10 @@ def output_student_list(manager, Slist):
     for i, student in enumerate(Slist):
         print(f"{i+1}. {student}")
 
-    overall_gpa = manager.calculate_gpa(Slist)
+    print(f"-----------------------------")
+    print(f"TOTAL students: {len(Slist)} | Average GPA: {Average_GPA(Slist)}")
+    print(f"-----------------------------")
 
-    print(f"-----------------------------")
-    print(f"TOTAL students: {len(Slist)} | Average GPA: {round(overall_gpa,2)}")
-    print(f"-----------------------------")
 
 def main():
 
@@ -60,7 +60,7 @@ def main():
     manager = StudentManager(user_lg)
 
     # create a list to input and output student
-    Slist = manager.Output_Load()
+    Slist = manager.Loadfile()
 
     running = True
     while running:
@@ -74,7 +74,7 @@ def main():
         print(f"3/ Delete student info")
         print(f"4/ Search student")
         print(f"5/ Sorting student")
-        print(f"6/ Calculate GPA (Average GPA)")
+        print(f"6/ Classification GPA")
         print(f"7/ Exit and extract file to .txt file")
         print(f"\n")
 
@@ -95,16 +95,12 @@ def main():
             case 5:
                 manager.sorting(Slist)
             case 6:
-                overall_gpa = manager.calculate_gpa(Slist)
-                print(f"Average GPA all student: {round(overall_gpa,2)}")
+                manager.classification_gpa(Slist)
             case 7:
-                manager.Input_Load(Slist)
+                manager.Savefile(Slist)
                 print("Programming exit...")
                 running = False
 
 
 if __name__ == "__main__":
     main()
-
-
-
